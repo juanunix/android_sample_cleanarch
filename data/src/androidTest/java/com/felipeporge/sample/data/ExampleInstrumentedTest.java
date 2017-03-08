c1b2d288704cc10459a7d95f52dade7f1f114a99
Felipe Porge Xavier <felipefpx@gmail.com>
2017-03-08 08:19:26 -0300

    Fixing packages

--- data/src/androidTest/java/com/felipeporge/sample/data/ExampleInstrumentedTest.java
+++ data/src/androidTest/java/com/felipeporge/sample/data/ExampleInstrumentedTest.java
@@ -1,4 +1,5 @@
-package com.crossover.felipeporge.auction;
+package com.felipeporge.sample.data;
+
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -7,7 +8,7 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -21,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.crossover.felipeporge.auction", appContext.getPackageName());
+        assertEquals("com.crossover.felipeporge.auction.test", appContext.getPackageName());
     }
 }
